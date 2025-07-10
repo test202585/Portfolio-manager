4a64cb438346a13567e86a205518290d9432a554
vikram somai <vikramsomai4@gmail.com>
2025-07-10 15:21:52 +0530

    new api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://13.51.109.157/api"
+const API_BASE_URL = "https://fd5efaa6e6ae.ngrok-free.app/"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
