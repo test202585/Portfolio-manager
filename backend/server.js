3b70306cc0a4e914dbabece3c9ec9c510f1d6843
vikram somai <vikramsomai4@gmail.com>
2025-07-10 14:00:07 +0530

    fix api

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const bcrypt = require("bcryptjs")
 // Middleware
 app.use(helmet())
 app.use(morgan("combined"))
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 
