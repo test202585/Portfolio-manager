b06e48cf4e8f5f1a813d1835b57075db7bd71551
vikram somai <vikramsomai4@gmail.com>
2025-07-10 19:01:26 +0530

    fix new api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://fd5efaa6e6ae.ngrok-free.app/api"
+const API_BASE_URL = "https://portfolio-2025.duckdns.org/api"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
